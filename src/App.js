24e1c1451996efc0f2dd615aeaf8d264de2f6d95
GeloSarmiento <124024402+GeloSarmiento@users.noreply.github.com>
2023-02-02 15:14:49 +0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './Header';
+import Dashboard from './Dashboard';
+import Footer from './Footer';
+import Menu from './Menu';
+
+function App() {
+  return (
+    <div class="wrapper">
+     <Header/>
+     <Dashboard/>
+     <Footer/>
+     <Menu/>
+    </div>
+  );
+}
+
+export default App;
